76211b5f3c584e3dfeaa92a76cb5bd0f5df74928
Sudhir Jonathan <sudhir.j@gmail.com>
2017-10-29 00:06:49 +0530

    Added and styled tagging

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,18 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.where(published: true)
+    @tags = []
   end
 
   def show
     @post = Post.find_by url: params[:path]
+    return if @post.present?
+    @posts = Post.all.where("tags @> ARRAY[?]::varchar[]", [params[:path]])
+    if @posts.count > 0
+      @tags = [params[:path]]
+      render :index
+      return
+    end
+    redirect_to root_path
   end
 end
